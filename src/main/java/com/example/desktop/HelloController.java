c253ee414d9f59775481d5995a830ea6de3148e0
Ullas Biswas Shontu <shontu2207086@stud.kuet.ac.bd>
2025-12-14 23:52:36 +0600

    page 2 choose category connected

--- src/main/java/com/example/desktop/HelloController.java
+++ src/main/java/com/example/desktop/HelloController.java
@@ -2,29 +2,51 @@ package com.example.desktop;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class HelloController {
     @FXML
     private Label welcomeText;
     @FXML
     private Button btnAdminLogin;
-
     @FXML
     private Button btnBookRoom;
+
     @FXML
     void handleAdminLogin(ActionEvent event) {
-
+        System.out.println("Admin Login Clicked");
     }
 
     @FXML
     void handleBookRoom(ActionEvent event) {
+        try {
 
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("book1.fxml"));
+            Parent root = loader.load();
+
+            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("Error: Could not load book1.fxml.");
+        }
     }
 
     @FXML
     public void initialize() {
+
         btnBookRoom.setOnAction(this::handleBookRoom);
         btnAdminLogin.setOnAction(this::handleAdminLogin);
     }
---
+++ src/main/java/com/example/desktop/MyController.java
@@ -0,0 +1,17 @@
+package com.example.desktop;
+
+import javafx.fxml.FXML;
+import javafx.event.ActionEvent;
+
+public class MyController {
+
+    @FXML
+    void handleSingleRoom(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleApartment(ActionEvent event) {
+
+    }
+}
